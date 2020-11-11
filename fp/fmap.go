545a04f132ad6927505a0534d24c7049a8ba1f17
Andrew Proshkin <zx80live@gmail.com>
2020-11-11 18:21:07 +0300

    autogenerated code

--- fp/fmap.go
+++ fp/fmap.go
@@ -1,33 +1,35 @@
 package fp
 
-func (l FListInt) MapInt(f FunctorInt) FListInt {
-  return FListInt {
-    head : l.head,
-    tail : l.tail,
-    functor : func(e Element) Element {
-      if processed := l.functor(e); processed != nil {
-        return f(processed.(int))
-      } else {
-        return nil
-      }
-    },
+/*
+  func (l FListInt) MapInt(f FunctorInt) FListInt {
+    return FListInt {
+      head : l.head,
+      tail : l.tail,
+      functor : func(e Element) Element {
+        if processed := l.functor(e); processed != nil {
+                                                         return f(processed.(int))
+                                                         } else {
+                                                                                     return nil
+                                                                                     }
+      },
+    }
   }
-}
 
-func (l FListInt) MapString(f FunctorIntString) FListString {
-  panic("")
-}
+  func (l FListInt) MapString(f FunctorIntString) FListString {
+    panic("")
+  }
 
-func foo() {
-  var l1 FListInt = FListInt { 0, nil, EmptyFunctor }
+  func foo() {
+    var l1 FListInt = FListInt { 0, nil, EmptyFunctor }
 
-  l1.MapInt(func(e int) int {
-    return e * 10
-  }).MapString(func(e int) string {
-    return "hello"
-  })
+    l1.MapInt(func(e int) int {
+      return e * 10
+    }).MapString(func(e int) string {
+      return "hello"
+    })
 
-  l1.MapString(func(e int) string {
-    return "hello"
-  })
-}
+    l1.MapString(func(e int) string {
+      return "hello"
+    })
+  }
+*/
