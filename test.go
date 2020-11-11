435cba9863307a8b5dde81d4760e0b595e52d994
Andrew Proshkin <zx80live@gmail.com>
2020-11-11 23:00:36 +0300

    map (non-optimized)

--- test.go
+++ test.go
@@ -9,7 +9,10 @@ func main() {
 	fmt.Println("Hello")
 
 	l := MakeListInt(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-	l.Filter(func(e int) bool { return e%2 == 0 }).Foreach(func(e int) {
-		fmt.Println(">", e)
-	})
+	l.
+		Filter(func(e int) bool { return e%2 == 0 }).
+		MapString(func(e int) string { return fmt.Sprintf("~~%v~~", e) }).
+		Foreach(func(e string) {
+			fmt.Println(">", e)
+		})
 }
