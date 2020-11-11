8802516971ab0cfa7db5b868b8497b92255d262e
Andrew Proshkin <zx80live@gmail.com>
2020-11-11 22:51:48 +0300

    filter (non-optimized)

--- test.go
+++ test.go
@@ -8,8 +8,8 @@ import (
 func main() {
 	fmt.Println("Hello")
 
-	l := MakeListInt(1, 2, 3, 4, 5).Cons(10).Cons(20).Cons(30)
-	l.Foreach(func(e int) {
+	l := MakeListInt(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	l.Filter(func(e int) bool { return e%2 == 0 }).Foreach(func(e int) {
 		fmt.Println(">", e)
 	})
 }
