72e1d4dbdd92b521c033f5f65f174b9c839bafd3
Andrew Proshkin <zx80live@gmail.com>
2020-11-12 23:12:06 +0300

    predicate bool logic

--- test.go
+++ test.go
@@ -8,9 +8,12 @@ import (
 func main() {
 	fmt.Println("Hello")
 
-	l := MakeListInt(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
+	l := MakeListInt(1, 2, 3, 4, 5, 6, -7, 8, 9, -10)
+	var even PredicateInt = func(e int) bool { return e%2 == 0 }
+	var pos PredicateInt = func(e int) bool { return e > 0 }
+
 	l.
-		Filter(func(e int) bool { return e%2 == 0 }).
+		Filter(even.And(pos)).
 		MapString(func(e int) string { return fmt.Sprintf("~~%v~~", e) }).
 		Foreach(func(e string) {
 			fmt.Println(">", e)
