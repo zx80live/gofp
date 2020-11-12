a4dad6aa0829fed5837198347c49fde3fc722c1a
Andrew Proshkin <zx80live@gmail.com>
2020-11-12 23:43:19 +0300

    isdefined/isempty

--- test.go
+++ test.go
@@ -21,6 +21,6 @@ func main() {
 
 	var i1 OptionInt = Int(1)
 	var i2 OptionInt = NoneInt
-	fmt.Println(i1.ToString())
-	fmt.Println(i2.ToString())
+	fmt.Println(i1.ToString(), i1.IsDefined(), i1.IsEmpty())
+	fmt.Println(i2.ToString(), i2.IsDefined(), i2.IsEmpty())
 }
