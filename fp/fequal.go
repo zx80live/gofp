6ad51f9daf4142caa3db94de0e02a60d1458203c
Andrey Proshkin <zx80live@gmail.com>
2020-11-14 17:17:12 +0300

    optional equal

--- fp/fequal.go
+++ fp/fequal.go
@@ -262,3 +262,563 @@ func EqualAnyArr(a, b []Any) bool {
 	}
 	return true
 }
+
+func EqualOptionBool(a, b OptionBool) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualBool(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionRune(a, b OptionRune) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualRune(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionByte(a, b OptionByte) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualByte(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionInt(a, b OptionInt) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualInt(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionInt8(a, b OptionInt8) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualInt8(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionInt16(a, b OptionInt16) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualInt16(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionInt32(a, b OptionInt32) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualInt32(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionInt64(a, b OptionInt64) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualInt64(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUint(a, b OptionUint) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUint(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUint8(a, b OptionUint8) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUint8(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUint16(a, b OptionUint16) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUint16(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUint32(a, b OptionUint32) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUint32(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUint64(a, b OptionUint64) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUint64(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUintptr(a, b OptionUintptr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUintptr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionFloat32(a, b OptionFloat32) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualFloat32(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionFloat64(a, b OptionFloat64) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualFloat64(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionComplex64(a, b OptionComplex64) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualComplex64(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionComplex128(a, b OptionComplex128) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualComplex128(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionString(a, b OptionString) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualString(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionAny(a, b OptionAny) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualAny(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionBoolArr(a, b OptionBoolArr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualBoolArr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionRuneArr(a, b OptionRuneArr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualRuneArr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionByteArr(a, b OptionByteArr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualByteArr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionIntArr(a, b OptionIntArr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualIntArr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionInt8Arr(a, b OptionInt8Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualInt8Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionInt16Arr(a, b OptionInt16Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualInt16Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionInt32Arr(a, b OptionInt32Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualInt32Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionInt64Arr(a, b OptionInt64Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualInt64Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUintArr(a, b OptionUintArr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUintArr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUint8Arr(a, b OptionUint8Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUint8Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUint16Arr(a, b OptionUint16Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUint16Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUint32Arr(a, b OptionUint32Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUint32Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUint64Arr(a, b OptionUint64Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUint64Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionUintptrArr(a, b OptionUintptrArr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualUintptrArr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionFloat32Arr(a, b OptionFloat32Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualFloat32Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionFloat64Arr(a, b OptionFloat64Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualFloat64Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionComplex64Arr(a, b OptionComplex64Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualComplex64Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionComplex128Arr(a, b OptionComplex128Arr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualComplex128Arr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionStringArr(a, b OptionStringArr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualStringArr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
+
+func EqualOptionAnyArr(a, b OptionAnyArr) bool {
+	if a.IsDefined() {
+		if b.IsDefined() {
+			return EqualAnyArr(*a.value, *b.value)
+		} else {
+			return false
+		}
+	} else if b.IsDefined() {
+		return false
+	} else {
+		return true
+	}
+}
