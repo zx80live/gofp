110b717645eed947f1b2b050cd00ed0b85fd3ad4
Andrey Proshkin <zx80live@gmail.com>
2020-12-09 20:53:46 +0300

    Array.Zip

--- test.go
+++ test.go
@@ -8,17 +8,5 @@ import (
 func main() {
 	fmt.Println("Hello")
 
-	l1 := MakeIntList(1, 2, 3)
-	l2 := MakeStringList("a", "b", "c")
-	l3 := Float32Array([]float32{0.5, 0.7, 0.9})
-
-	fmt.Println(l1.ZipStringList(l2).ZipFloat32Array(l3).ToString())
-
-	fmt.Println(
-		MakeIntList(1, 2, 3).
-			ZipAllStringList(
-				MakeStringList("a", "b", "c", "d"), -100, "NONE").
-			ToString())
-
-	fmt.Println(StringArray([]string{"a", "b", "c"}).ZipWithIndex().ToString())
+	fmt.Println(IntArray([]int{10, 20, 30}).ZipStringArray(StringArray([]string{"a"})).ToString())
 }
