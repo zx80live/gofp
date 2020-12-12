26fa76e70b81c3a178a173fdd37ad8542420c7e8
Andrey Proshkin <zx80live@gmail.com>
2020-12-12 21:30:01 +0300

    queue map

--- test.go
+++ test.go
@@ -6,11 +6,18 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello")
+	q := MkStringQueue("a", "b", "c").Enqueue("d").Enqueue("e").Enqueue("f")
+	q.Foreach(func(e string) { fmt.Println(e) })
+	fmt.Println(q.ToList().ToString())
 
-	fmt.Println(IntArray([]int{10, 20, 30}).ZipAllStringArray(StringArray([]string{"a", "b", "c", "d"}), -100, "NONE").ToString())
+	q2 := MkIntQueue(1, 2, 3, 4, 5, 6, 7).Enqueue(8).Enqueue(9)
+	fmt.Println(q2.Filter(EvenInt).ToList().ToString())
 
-	t1 := Tuple2{10, "hello"}
-	t2 := Tuple2{t1, MkIntList(1, 2, 3)}
-	fmt.Println(t2.ToString())
+	fmt.Println(q.Reduce(func(a, b string) string { return fmt.Sprintf("%v-%v", a, b) }))
+	fmt.Println(q2.Reduce(func(a, b int) int { return a + b }))
+
+	fmt.Println(q.MapString(func(e string) string { return fmt.Sprintf("<%v>", e) }).ToList().ToString())
+
+	fmt.Println(q2.MapString(func(e int) string { return fmt.Sprintf("<%v>", e) }).ToList().ToString())
+
 }
